010de5fc6e7cb38c2ea31a7c4af3b8df8f5ceca9
Jonas Obrist <ojiidotch@gmail.com>
2012-09-19 20:11:49 +0200

    made it nicer

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
+import sys
 from django import forms
+import django
 from django.http import HttpResponse
 import base64
 from django.shortcuts import render_to_response
@@ -17,15 +19,21 @@ class PymagingForm(forms.Form):
     image = forms.ImageField()
 
 def index(request):
-    form = PymagingForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        form = PymagingForm(request.POST, request.FILES)
+    else:
+        form = PymagingForm()
     if form.is_valid():
         response = HttpResponse(content_type='image/png')
         image = Image.open(form.cleaned_data['image'])
         image = image.resize(200, 200)
         image.save(response, 'png')
         return response
-    return render_to_response('index.html', RequestContext(request, {'form': form}))
+    context = RequestContext(request)
+    context['form'] = form
+    context['python'] = sys.version
+    context['django'] = django.get_version()
+    return render_to_response('index.html', context)
 
 def favicon(request):
     return HttpResponse(base64.b64decode(FAVICON), content_type='image/x-icon')
-
